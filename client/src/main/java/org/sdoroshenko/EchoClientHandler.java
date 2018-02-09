cf1ab58ddb49baeb682a236b559f8dba70e9d1a0
sdoroshenko <VieFP1l-K3Yw>
2018-02-09 09:02:06 +0300

    Samples submitted

--- client/src/main/java/org/sdoroshenko/EchoClientHandler.java
+++ client/src/main/java/org/sdoroshenko/EchoClientHandler.java
@@ -2,11 +2,16 @@ package org.sdoroshenko;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 /**
@@ -23,7 +28,46 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     public void channelRead0(ChannelHandlerContext ctx, ByteBuf byteBuf) throws Exception {
         System.out.println("Client received: " + byteBuf.toString(CharsetUtil.UTF_8));
-        ctx.write(Unpooled.copiedBuffer("Netty rocks! 2nd!\n", CharsetUtil.UTF_8));
+
+        final Channel channel = ctx.channel();
+        final ByteBuf buf = Unpooled.copiedBuffer("Netty rocks! 2nd!\n", CharsetUtil.UTF_8).retain();
+
+        //        ctx.write(buf);
+        writeToChannel(channel, buf);
+//        writeFromThreads(channel, buf);
+    }
+
+    private ChannelFuture writeToChannel(Channel channel, ByteBuf buf) {
+        ChannelFuture cf = channel.write(buf);
+        cf.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                if (future.isSuccess()) {
+                    System.out.println("Write successful");
+                } else {
+                    System.err.println("Write error");
+                    future.cause().printStackTrace();
+                }
+            }
+        });
+
+        return cf;
+    }
+
+    private void writeFromThreads(final Channel channel, final ByteBuf buf) {
+        Runnable writer = new Runnable() {
+            @Override
+            public void run() {
+                channel.writeAndFlush(buf.duplicate().retain());
+            }
+        };
+        Executor executor = Executors.newCachedThreadPool();
+
+        // write in one thread
+        executor.execute(writer);
+
+        // write in another thread
+        executor.execute(writer);
     }
 
     @Override
